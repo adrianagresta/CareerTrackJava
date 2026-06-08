a90d9dc2ae12ac1d7672ee181d05098ffd0f1b68
Adrian Agresta <adrian.agresta@outlook.com>
2026-06-08 15:38:45 -0500

    Initial revision.

---
+++ src/main/java/com/agresta/CareerTrack/CareerTrackJavaApplication.java
@@ -0,0 +1,13 @@
+package com.agresta.CareerTrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerTrackJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CareerTrackJavaApplication.class, args);
+	}
+
+}
